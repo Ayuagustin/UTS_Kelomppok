809dd98c12673d835dd4b7074b7b8909d6aff1c0
Ayuagustin <agustinayu818@gmail.com>
2021-11-13 01:45:24 +0700

    UTS Kelompok

---
+++ lib/register_view.dart
@@ -0,0 +1,252 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_splashscreen/constants.dart';
+
+class RegisterPage extends StatelessWidget {
+  static const routeName = "/registerPage";
+
+  const RegisterPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          children: <Widget>[
+            Center(
+              child: Column(
+                children: <Widget>[
+                  _iconRegister(),
+                  _titleDescription(),
+                  _textField(),
+                  _buildButton(context),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _iconRegister() {
+    return Image.asset(
+      "lib/images/logo.png",
+      width: 150.0,
+      height: 150.0,
+    );
+  }
+
+  Widget _titleDescription() {
+    return Column(
+      // ignore: prefer_const_literals_to_create_immutables
+      children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        const Text(
+          "Registrasi",
+          style: TextStyle(
+            color: Colors.green,
+            fontSize: 16.0,
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+      ],
+    );
+  }
+
+  Widget _textField() {
+    return Column(
+      children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+
+//
+
+        TextFormField(
+          decoration: const InputDecoration(
+            icon: Icon(
+              Icons.person,
+              color: Colors.green,
+            ),
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlineTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.green,
+                width: 3.0,
+              ),
+            ),
+            hintText: "NIM",
+            hintStyle: TextStyle(color: ColorPalette.hintColor),
+          ),
+          style: const TextStyle(color: Colors.green),
+          autofocus: false,
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            icon: Icon(
+              Icons.person,
+              color: Colors.green,
+            ),
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlineTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.green,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Nama Lengkap",
+            hintStyle: TextStyle(color: ColorPalette.hintColor),
+          ),
+          style: const TextStyle(color: Colors.green),
+          autofocus: false,
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            icon: Icon(
+              Icons.mail,
+              color: Colors.green,
+            ),
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlineTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.green,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Prodi",
+            hintStyle: TextStyle(color: ColorPalette.hintColor),
+          ),
+          style: const TextStyle(color: Colors.green),
+          autofocus: false,
+        ),
+
+        TextFormField(
+          decoration: const InputDecoration(
+            icon: Icon(
+              Icons.lock_outlined,
+              color: Colors.green,
+            ),
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlineTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.green,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Password",
+            hintStyle: TextStyle(color: ColorPalette.hintColor),
+          ),
+          style: const TextStyle(color: Colors.green),
+          obscureText: true,
+          autofocus: false,
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            icon: Icon(
+              Icons.password_outlined,
+              color: Colors.green,
+            ),
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlineTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.green,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Confirm Password",
+            hintStyle: TextStyle(color: ColorPalette.hintColor),
+          ),
+          style: const TextStyle(color: Colors.green),
+          obscureText: true,
+          autofocus: false,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildButton(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        InkWell(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            width: double.infinity,
+            child: const Text(
+              'Registrasi',
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        const Text(
+          'atau',
+          style: TextStyle(
+            color: Colors.green,
+            fontSize: 12.0,
+          ),
+        ),
+        // ignore: deprecated_member_use
+        FlatButton(
+          child: const Text(
+            'Masuk',
+            style: TextStyle(color: Colors.green),
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, "/");
+          },
+        ),
+      ],
+    );
+  }
+}
